71659cea98c1805b0f033bf91085c193d43483f6
Jonathan Harrison <j.harrison@altitudeangel.com>
2017-09-04 08:17:13 +0100

    Working example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # typescript_npm_scripts
+
+This project demonstrates how to use [Typescript](https://www.typescriptlang.org/) for [npm scripts](https://docs.npmjs.com/misc/scripts).
---
+++ buildScripts/start.ts
@@ -0,0 +1,7 @@
+import { green, yellow } from "chalk";
+
+console.log("Performing actions from start script ...");
+
+console.log(yellow("warning ") + "Not really doing anything.");
+
+console.log(green("success ") + "Actions from start script complete.");
